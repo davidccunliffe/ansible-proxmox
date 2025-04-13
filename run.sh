ef4dab75c02e1becf8dfc4d0bf8a3ef32a11ac31
David Cunliffe <davidccunliffe@gmail.com>
2025-04-12 20:41:33 -0400

    adding cifs storage

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# ansible-playbook -i inventories/home-lab/hosts.yml playbooks/setup.yml
+# ansible-playbook -i inventories/home-lab/hosts.yml playbooks/setup.yml --ask-vault-pass
 ansible-playbook ./playbooks/setup.yml -i ./inventories/${1} -t ${2:-all}
